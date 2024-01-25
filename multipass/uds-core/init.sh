802ee4bf6031a4342a71666f335f9a3eabd4c499
James Allen <10216753+jamesallen-atx@users.noreply.github.com>
2024-01-25 14:53:44 -0600

    reload system files

--- multipass/uds-core/init.sh
+++ multipass/uds-core/init.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-multipass launch --name uds-core --cpus 6 --memory 8G --disk 60G --cloud-init ./cloud-config.yaml 22.04
+multipass launch --name uds-core --cpus 6 --memory 16G --disk 60G --cloud-init ./cloud-config.yaml 22.04
